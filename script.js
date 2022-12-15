6744c8c93328545324e066fc24bab75fc73b59fd
Martin Dallos <tinypin14@gmail.com>
2022-12-15 14:42:38 +0100

    Created first version of 1000m timer

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const geoLoc = (function() {
-    const getLocationData = () => {
-        navigator.geolocation.getCurrentPosition(success => console.log(success.coords.speed));
-    }
+const speedIndicatorH1 = document.querySelector('.speed-indication');
+function getAndUpdateSpeed() {
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        // Do something with the position object
+        const currLocationData = position.coords;
+        const currLocationSpeed = currLocationData.speed === null ? '0' : currLocationData.speed;
+        // calculate time to go 1000m
+        const timeToGo1000m = 1000 / currLocationSpeed * 3.6;
+        speedIndicatorH1.textContent = timeToGo1000m;
+        console.log(currLocationSpeed);
+        console.log(position.coords);
+      },error => console.log(error), {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      });
+      
+  } else {
+    // Geolocation is not supported by the browser
+    console.log('Geolocation is not supported by the browser');
+  }
+}
 
-    return {
-        getLocationData,
-    }
-})() 
-console.log(geoLoc.getLocationData());
+function updateSpeedAndRead() {
+  setInterval(getAndUpdateSpeed, 5000);
+
+}
+updateSpeedAndRead();
